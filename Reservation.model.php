28f5444bcef7d3e0b12e4cf23195d77fcdc72f2c
richardR144 <richard.rigollet@lapiscine.pro>
2025-04-14 16:21:25 +0200

    constructeur (new) plus arguments passés en paramêtres

--- Reservation.model.php
+++ Reservation.model.php
@@ -1,11 +1,8 @@
 <?php
 
-require_once "../config/config.php";
 
-// Reservation.model.php
-
-class Reservation {
-
+class Reservation
+{
     public $name;
     public $email;
     public $place;
@@ -18,27 +15,39 @@ class Reservation {
     public $cleaningOption;
     public $cleaningPrice = 5000; // prix de nettoyage
 
-public function __construct() {
-    $this->name = "Evil";
-    $this->email = "evil@evil.com";
-    $this->place = "Mooréa";
-    $this->startDate = new DateTime("2025-10-01");
-    $this->endDate = new DateTime("2025-11-10");
-    $this->nightPrice = 1000;
-    $this->cleaningOption = true;
-    $this->bookedAt = new DateTime();
-    $this->status = "CART";
+        public function __construct($name, $email, $place, $startDate, $endDate, $nightPrice, $status, $bookedAt, $cleaningOption, $cleaningPrice)
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->place = "Mooréa";
+        $this->startDate = new DateTime("2025-10-01");
+        $this->endDate = new DateTime("2025-11-10");
+        $this->nightPrice = 1000;
+        $this->cleaningOption = true;
+        $this->bookedAt = new DateTime();
+        $this->status = "CART";
 
 
 
-    // valeurs calculées automatiquement
-$this->totalPrice = (($this->endDate->getTimestamp() - $this->startDate->getTimestamp()) / (3600 * 24) * $this->nightPrice) + $this->cleaningPrice;
-
+        // valeurs calculées automatiquement
+        $this->totalPrice = (($this->endDate->getTimestamp() - $this->startDate->getTimestamp()) / (3600 * 24) * $this->nightPrice) + $this->cleaningPrice;
+    }
 }
-}
 
 
-$reservation = new Reservation();
+$name = "evil"; // nom de réservation
+$email = "evil@evil.com"; // adresse e-mail de réservation
+$place = "Mooréa"; // lieu de réservation
+$startDate = new DateTime("2025-10-01");
+$endDate = new DateTime("2025-11-10");
+$nightPrice = 1000; // prix par nuit
+$status = "CART"; // statut de réservation
+$bookedAt = new DateTime(); // date de réservation
+$cleaningOption = true; // option de nettoyage, j'ai choisi vrai
+$cleaningPrice = 5000; // prix de nettoyage
+
+// Création d'une instance de la classe Reservation, sachant que le new fait référence au constructeur
+// et que le constructeur est appelé automatiquement lors de la création de l'objet
+$reservation = new Reservation($name, $email, $place, $startDate, $endDate, $nightPrice, $status, $bookedAt, $cleaningOption, $cleaningPrice);
 
 var_dump($reservation);
-
