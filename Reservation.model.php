ac76734e437c7b67434cf5c02cb2510641ef893d
richardR144 <richard.rigollet@lapiscine.pro>
2025-04-14 17:19:40 +0200

    méthode canceled

--- Reservation.model.php
+++ Reservation.model.php
@@ -14,25 +14,40 @@ class Reservation
     public $bookedAt;
     public $cleaningOption;
     public $cleaningPrice = 5000; // prix de nettoyage
-        //creation 
+        //creation de la fonction _constucteur
+        // le constructeur est une méthode spéciale qui est appelée automatiquement lors de la création d'un objet 
         public function __construct($name, $email, $place, $startDate, $endDate, $cleaningOption)
     {
         $this->name = $name; // nom de réservation
         $this->email = $email;
-        $this->place = "Mooréa";
-        $this->startDate = new DateTime("2025-10-01");
-        $this->endDate = new DateTime("2025-11-10");
-        $this->nightPrice = 1000;
+        $this->place = $place;;
+        $this->startDate = $startDate;
+        $this->endDate = $endDate;
+        $this->nightPrice = 10000; // prix de la nuit
         $this->cleaningOption = true;
         $this->bookedAt = new DateTime();
         $this->status = "CART";
-
+       
 
 
         // valeurs calculées automatiquement
         $this->totalPrice = (($this->endDate->getTimestamp() - $this->startDate->getTimestamp()) / (3600 * 24) * $this->nightPrice) + $this->cleaningPrice;
+        $this->status = "CART"; // statut de réservation
+        $this->bookedAt = new DateTime(); // date de réservation
+       
     }
-}
+
+        // méthode pour changer le statut de la réservation
+            public function cancel()
+        {
+            if ($this->status === "CART")
+        {   // statut de réservation par carte
+            $this->status = "CANCELLED"; // methode d'annulation si carte
+        }
+    }
+}        
+
+
 
 
 $name = "evil"; // nom de réservation
@@ -40,11 +55,15 @@ $email = "evil@evil.com"; // adresse e-mail de réservation
 $place = "Mooréa"; // lieu de réservation
 $startDate = new DateTime("2025-10-01");
 $endDate = new DateTime("2025-11-10");
-$cleaningOption = true; // option de nettoyage, j'ai choisi vrai
+$cleaningOption = false; // option de nettoyage, j'ai choisi faux
 
 
 // Création d'une instance de la classe Reservation, sachant que le new fait référence au constructeur
 // et que le constructeur est appelé automatiquement lors de la création de l'objet
 $reservation = new Reservation($name, $email, $place, $startDate, $endDate, $cleaningOption);
+
+
+
+    
 
 var_dump($reservation);
