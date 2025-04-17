e4e39ce967166abec1d7b26f8f5b10e1f903a90a
richardR144 <richard.rigollet@lapiscine.pro>
2025-04-17 15:09:45 +0200

    connectToDB

--- config/config.php
+++ config/config.php
@@ -6,13 +6,17 @@ error_reporting(E_ALL);
 //gérer les problèmes liés à la session avec le navigateur évite les warnings
 session_start();  
 
-function connectToDB() {
-    try {
+function connectToDB() {  // Connexion à la base de données
+    try {  // PDO est une extension de PHP qui permet d'accéder à une base de données
         $pdo = new PDO('mysql:host=localhost:3306;dbname=resa-php-tp', 'root', 'root');
+        // On active le mode d'erreur d'exception
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $pdo;
-    } catch (PDOException $e) {
+        
+        return $pdo;  // sinon on retourne l'objet PDO
+    } catch (PDOException $e) {  // Si une erreur se produit lors de la connexion à la base de données, on l'attrape ici
+        // On affiche un message d'erreur 
         throw new Exception('Erreur de connexion à la base de données : ') . $e->getMessage();
+        // On arrête l'exécution du script
         exit;
     }
 }
--- model/reservation-repository.php
+++ model/reservation-repository.php
@@ -1,19 +1,44 @@
 <?php
 
-function persistReservation($reservation) {
-    session_start();
+function persistReservation($reservation) {  // je persiste la réservation dans la base de données et je vérifie si la réservation existe déjà
+    session_start();  // je démarre la session
 
     $_SESSION['reservation'] = $reservation; // je stocke la réservation dans la session
 
+    //requête qui prend en charge la réservation dans la base de données
+    $pdo = connectToDB();  // je me connecte à la base de données
+
+    $startDateFormatted = $reservation->startDate->format('Y-m-d H:i:s');  // je formate la date de début
+    $endDateFormatted = $reservation->endDate->format('Y-m-d H:i:s');      // je formate la date de fin
+    $bookedAtFormatted = $reservation->bookedAt->format('Y-m-d H:i:s');    // je formate la date de réservation
+
+    $query = "INSERT INTO reservation   # je prépare la requête d'insertion et je précise les champs de la table de reservation
+    (name, place, start_date, end_date, cleaning_option, night_price, total_price, booked_at, status) 
+     VALUES 
+     (
+        '$reservation->name',
+        '$reservation->place', 
+        '$startDateFormatted', 
+        '$endDateFormatted', 
+        '$reservation->cleaning_option', 
+        '$reservation->night_price', 
+        '$reservation->total_price', 
+        '$bookedAtFormatted', 
+        '$reservation->status'
+     )";
+      //var_dump($query); // je vérifie la requête
+
+      $pdo->$query($query); // j'exécute la requête
 }
 
 
-function findReservationForUser() {
-    session_start();
+function findReservationForUser()   // je cherche la réservation de l'utilisateur dans la base de données
+{
+    session_start(); // je démarre la session
 
-    if ((array_key_exists('reservation', $_SESSION))) {
-        return $_SESSION['reservation']; // je récupère la réservation de l'utilisateur
+    if ((array_key_exists('reservation', $_SESSION))) { // je vérifie si la réservation existe dans la session
+        return $_SESSION['reservation'];                // je retourne la réservation de l'utilisateur
     } else {
-        return null; // si pas de réservation, je retourne null
+        return null;                                    // si pas de réservation, je retourne null
     }
 }
