052bc0549c906aaaedd2e2a341d15e601e68d0a8
richardR144 <richard.rigollet@lapiscine.pro>
2025-04-17 13:53:27 +0200

    fonction PDO

--- config/config.php
+++ config/config.php
@@ -5,4 +5,16 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 //gérer les problèmes liés à la session avec le navigateur évite les warnings
 session_start();  
-?>
+
+function connectToDB() {
+    try {
+        $pdo = new PDO('mysql:host=localhost:3306;dbname=resa-php-tp', 'root', 'root');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch (PDOException $e) {
+        throw new Exception('Erreur de connexion à la base de données : ') . $e->getMessage();
+        exit;
+    }
+}
+
+
